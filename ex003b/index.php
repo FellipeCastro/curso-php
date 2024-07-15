41c9bfe9a4dcf53b2a758c626fee954a8910fcb9
Fellipe Castro <fellipecastro27d@gmail.com>
2024-07-15 18:15:29 -0300

    Aula 04 finalizada

---
+++ ex003b/index.php
@@ -0,0 +1,16 @@
+<?php 
+    $nome = "Fellipe";
+    $sobrenome = "Castro";
+    const CANAL = "Curso em Vídeo \u{1F499}";
+
+    echo "$nome $sobrenome \u{1F596}";
+    echo "<br/>";
+    echo '$nome $sobrenome \u{1F596}';
+    echo "<br/>";
+    echo "Eu adoro o " . CANAL;
+    echo "<br/>";
+    echo "Estamos no ano " . date('Y');
+    echo "<br/>";
+    echo "$nome \"Monstruoso\" $sobrenome";
+    echo "<br/>";
+?>
---
+++ ex004/cad.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Apresente-se para nós!</h1>
+    </header>
+    <main>
+        <?php 
+            $nome = $_GET["nome"] ?? "Nome";
+            $sobrenome = $_GET["sobrenome"] ?? "Sobrenome";
+
+            echo "<p>Seja bem vindo(a) <strong>$nome $sobrenome</strong>!</p>"
+        ?>
+
+        <p><a href="javascript:history.go(-1)">Voltar</a></p>
+    </main>
+</body>
+</html>
